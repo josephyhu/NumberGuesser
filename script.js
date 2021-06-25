ff4fbf0761fb21521ace4bfe9e1cffdc26688578
Joseph Yhu <josephsyhu@gmail.com>
2021-06-24 20:42:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ let currentRoundNumber = 1;
 // Write your code below:
 const generateTarget = () => Math.floor(Math.random() * 10);
 
+const getAbsoluteDistance = (num1, num2) => Math.abs(num1 - num2);
+
 const compareGuesses = (user, computer, target) => {
-    if (Math.abs(user - target) <= Math.abs(computer - target)) {
+    if (user < 0 || user > 9) {
+        alert('The number is out of range.');
+        return;
+    }
+    if (getAbsoluteDistance(user, target) <= getAbsoluteDistance(computer, target)) {
         return true;
     } else {
         return false;
